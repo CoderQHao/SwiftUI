5dbe63a061a60460458c220f587029267f1e4904
DongQing <haoqing@dushu365.com>
2023-09-15 15:41:51 +0800

    feat:Group

---
+++ 03_页面布局/01_Group/DemoProject/ContentView.swift
@@ -0,0 +1,33 @@
+//
+//  ContentView.swift
+//  DemoProject
+//
+//  Created by DongQing on 2023/9/11.
+//
+
+import SwiftUI
+
+struct ContentView : View {
+    
+    var body: some View {
+        Group {
+            Text("Apple")
+            Text("Banana")
+            Text("Orange")
+            Text("Watermelon")
+            Text("Grape")
+            Text("Papaya")
+            Text("Pear")
+        }
+        .font(.title)
+        .foregroundColor(.orange)
+    }
+}
+
+#if DEBUG
+struct ContentView_Previews : PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
+#endif
