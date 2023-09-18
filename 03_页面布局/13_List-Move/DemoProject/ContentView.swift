860fa2d7ff0afb8985a878d4832fa83ec87d97c0
DongQing <haoqing@dushu365.com>
2023-09-18 19:48:12 +0800

    feat:List-Move

---
+++ 03_页面布局/13_List-Move/DemoProject/ContentView.swift
@@ -0,0 +1,41 @@
+//
+//  ContentView.swift
+//  DemoProject
+//
+//  Created by DongQing on 2023/9/11.
+//
+
+import SwiftUI
+
+struct ContentView : View {
+    
+    @State var languages = ["Objective-C", "Swift", "SwiftUI"]
+
+    var body: some View {
+        NavigationView {
+            List {
+                ForEach(languages, id: \.self) {
+                    Text($0)
+                }
+                .onMove { fromOffsets, destination in
+                    self.languages.move(fromOffsets: fromOffsets, toOffset: destination)
+                }
+            }
+            .navigationBarTitle(Text("Edit Row"), displayMode: .large)
+            .navigationBarItems(trailing: EditButton())
+        }
+    }
+    
+    func addItem() {
+        languages.append("Flutter")
+    }
+}
+
+
+#if DEBUG
+struct ContentView_Previews : PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
+#endif
