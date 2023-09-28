783d1b4aead9646f87a5f6954ba83ca2e32c7269
DongQing <haoqing@dushu365.com>
2023-09-28 15:45:18 +0800

    feat:ObservableObject

---
+++ 06_页面跳转/11_ObservableObject/DemoProject/ContentView.swift
@@ -0,0 +1,50 @@
+//
+//  ContentView.swift
+//  DemoProject
+//
+//  Created by DongQing on 2023/9/11.
+//
+
+import SwiftUI
+
+class UserModel: ObservableObject{
+    @Published var nickName: String = ""
+}
+
+struct ContentView : View {
+    
+    @ObservedObject var model = UserModel()
+    @State var isPresented = false
+    
+    let dismiss = Alert.Button.default(Text("OK")) {}
+    
+    var alert: Alert {
+        Alert(title: Text("Your nickname"),
+              message: Text("\(self.model.nickName)"),
+              dismissButton: dismiss)
+    }
+    
+    var body: some View {
+        VStack {
+            TextField("Your nickname", text: $model.nickName)
+                .padding()
+            Button(action: {
+                self.isPresented = true
+            }, label: {
+                Text("Show")
+            })
+            .alert(isPresented: $isPresented) { () -> Alert in
+                alert
+            }
+        }
+    }
+}
+
+
+#if DEBUG
+struct ContentView_Previews : PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
+#endif
